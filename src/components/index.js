d07cfe89383ad9e4739f40ae963217165ec70b81
geokuu <qeorq25@gmail.com>
2024-01-09 21:13:30 +0200

    update

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar.jsx";
-export { default as Card } from "./Card.jsx";
-export { default as Cards } from "./Cards.jsx";
-export { default as CardsSection } from "./CardsSection.jsx";
-export { default as Categories } from "./Categories.jsx";
-export { default as Footer } from "./Footer.jsx";
-export { default as Intro } from "./Intro.jsx";
--- src/pages/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LandingPage } from "./LandingPage.jsx";
-export { default as ErrorPage } from "./ErrorPage.jsx";
